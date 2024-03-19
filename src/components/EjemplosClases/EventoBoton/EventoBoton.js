f971da1dafcf06706670200bff9112653e0a561a
SmaelNico <nicolas@nomadadigital.com.ar>
2024-03-18 21:35:58 -0300

    Ejemplo eventos

--- src/components/EjemplosClases/EventoBoton/EventoBoton.js
+++ src/components/EjemplosClases/EventoBoton/EventoBoton.js
@@ -1,8 +1,13 @@
 import React from "react";
 
-export const EventoBoton = () => {
+export const EventoBoton = ({ handleClick }) => {
+	// const handleClick = (evento) => {
+	// 	console.log(evento);
+	// };
+
 	return (
 		<button
+			onClick={() => handleClick(true)}
 			style={{
 				left: 0,
 				margin: "0 auto",
